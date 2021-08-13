6cd84d76f3e104208fc9e700c7d3456f38fca1d3
talha riaz <talharulez85@gmail.com>
2021-08-13 23:59:38 +0500

    Work Complete

---
+++ public/app.js
@@ -0,0 +1,102 @@
+var list = document.getElementById("list")
+
+
+function addTodo(){
+
+    var todo_item = document.getElementById("todo-item")
+    
+    
+    var li = document.createElement("li")
+    var liText = document.createTextNode(todo_item.value)
+    li.appendChild(liText)
+    
+    var delBtn = document.createElement("button")
+    var delText = document.createTextNode("DELETE")
+    delBtn.setAttribute("class","btn") 
+    delBtn.setAttribute("onclick","deleteITem(this)") 
+    
+    delBtn.appendChild(delText)
+
+
+
+
+var editBtn = document.createElement("button")
+var editText = document.createTextNode("EDIT")
+editBtn.appendChild(editText)
+
+
+editBtn.setAttribute("onclick","edititem(this)")
+
+
+
+
+
+
+
+
+    li.appendChild(delBtn)
+
+   li.appendChild(editBtn)
+     
+    list.appendChild(li)
+   todo_item.value = ""
+    console.log(li);
+}
+
+
+
+function deleteITem(e){
+    
+    
+    e.parentNode.remove()
+}
+
+
+function edititem(e){
+var val = prompt ("Enter updated value ", e.parentNode.firstChild.nodeValue )
+
+ 
+e.parentNode.firstChild.nodeValue = val
+}
+
+function deleteAll(){
+    list.innerHTML = ""
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
